698da234c464e29051779c7fb6cc09e3c52d2560
Andrej Petras <andrej@lorislab.org>
2025-04-30 11:11:25 +0200

    feat: add parameters

---
+++ imports/parameters/import-parameters.sh
@@ -0,0 +1,16 @@
+echo "##### Importing parameters"
+
+for entry in "."/*.json
+do
+  #echo "$entry"
+  file=$(basename "$entry")
+  file=`echo $file | cut -d '.' -f 1`
+  product=${file%%_*}
+  appid=`echo $file | cut -d'_' -f2`
+  status_code=`curl --write-out %{http_code} --silent --output /dev/null -X PUT -H 'Content-Type: application/json' "http://onecx-parameters-svc/operator/v1/parameters/$product/$appid" -d @$entry`
+  if [[ "$status_code" =~ (200|201)$  ]]; then
+    echo -e "${GREEN}Parameters for app $appid and product $product was uploaded with result $status_code ${NC}"
+  else
+    echo -e "${RED}Parameters for app $appid and product $product was uploaded with result $status_code ${NC}"
+  fi 
+done
