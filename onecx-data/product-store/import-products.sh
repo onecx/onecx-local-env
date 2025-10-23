0d69524b026e86f9bbc659ed2581b4b0f7c94a4f
Henry Taeschner <github@henry-taeschner.de>
2025-10-23 06:00:24 +0200

    feat: update products and slots

--- onecx-data/product-store/import-products.sh
+++ onecx-data/product-store/import-products.sh
@@ -7,10 +7,9 @@ echo -e "${CYAN}Importing Products in Product Store ${NC}"
 
 for entry in "./products"/*
 do
-  #echo "entry: $entry"
   file_name=$(basename "$entry")
   file_name=`echo $file_name | cut -d '.' -f 1`
-  #echo "file name: $file_name"
+  #echo "entry: $entry, file name: $file_name"
   status_code=`curl --write-out %{http_code} --silent --output /dev/null -X PUT -H 'Content-Type: application/json' "http://onecx-product-store-svc/operator/product/v1/update/$file_name" -d @$entry`
 
   if [[ "$status_code" =~ (200|201)$  ]]; then
