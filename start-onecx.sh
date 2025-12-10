27810e164137288b1dfc588a884d37d9b38246a5
Henry Taeschner <github@henry-taeschner.de>
2025-12-10 19:11:26 +0100

    feat: fix tenant id enablement

--- start-onecx.sh
+++ start-onecx.sh
@@ -37,7 +37,13 @@ usage_short () {
   Usage: $0  [-hsx] [-e <edition>] [-p <profile>]
 USAGE
 }
-
+#################################################################
+## Enable secure authentication
+enable_security () {
+  SECURITY=true
+  SECURITY_AUTH_USED=yes
+  SECURITY_TENANT_ID_ENABLED=true
+}
 
 #################################################################
 ## Defaults
@@ -47,6 +53,7 @@ SECURITY=false
 SECURITY_AUTH_USED=no
 SECURITY_TENANT_ID_ENABLED=false
 IMPORT=yes
+ENV_FILE="versions/$EDITION/.env"
 
 
 #################################################################
@@ -72,8 +79,7 @@ while getopts ":he:p:sx" opt; do
           PROFILE=$OPTARG
         fi
         ;;
-    s ) SECURITY=true
-        SECURITY_TENANT_ID_ENABLED=true
+    s ) enable_security
         ;;
     x ) IMPORT=no
         ;;
@@ -88,20 +94,20 @@ done
 
 #################################################################
 ## Security Authentication enabled?
-ENV_FILE="versions/$EDITION/.env"
 export OLE_SECURITY_AUTH_ENABLED=false
 
-
-if [ -f "$ENV_FILE" ]; then
-  security_enabled=$(grep -c "ONECX_SECURITY_AUTH_ENABLED=true" "$ENV_FILE")
+if [[ $SECURITY == "false" ]]; then
+  # read preset
+  if [ -f "$ENV_FILE" ]; then
+    security_enabled=$(grep "^ONECX_SECURITY_AUTH_ENABLED=" "$ENV_FILE" | cut -d '=' -f2)
+    if [[ $security_enabled == "true" ]]; then
+      enable_security
+    fi
+  fi
 fi
-if [[ ($security_enabled == 1) || ($SECURITY == "true") ]]; then
-  SECURITY_AUTH_USED=yes
-  export OLE_SECURITY_AUTH_ENABLED=true
-  SECURITY_TENANT_ID_ENABLED=true
-fi
 
 
+#################################################################
 #################################################################
 ## Start profile services
 printf "  edition: ${GREEN}$EDITION${NC}, profile: ${GREEN}$PROFILE${NC}, import: ${GREEN}$IMPORT${NC}, security authentication: ${GREEN}$SECURITY_AUTH_USED${NC}\n"
@@ -109,11 +115,12 @@ printf "  edition: ${GREEN}$EDITION${NC}, profile: ${GREEN}$PROFILE${NC}, import
 if [[ $# == 0 ]]; then
   usage_short
 fi
-# Using 'docker compose' (v2). If using older docker, change to 'docker-compose'
-ONECX_SECURITY_AUTH_ENABLED=${SECURITY}  TKIT_RS_CONTEXT_TENANT_ID_ENABLED=${SECURITY_TENANT_ID_ENABLED}  \
-   docker compose --profile $PROFILE  up -d
 
-# check success
+# Using 'docker compose' (v2). If using older docker, change to 'docker-compose'
+ONECX_SECURITY_AUTH_ENABLED=${SECURITY}  ONECX_RS_CONTEXT_TENANT_ID_ENABLED=${SECURITY_TENANT_ID_ENABLED}  \
+    docker compose --profile $PROFILE  up -d
+
+# Check running shell bff
 shell_is_healthy=`docker inspect --format='{{.State.Health.Status}}'  onecx-shell-bff`
 
 
@@ -128,7 +135,7 @@ if [[ $shell_is_healthy == "healthy" && $IMPORT == "yes" ]]; then
     printf "${RED}Error: import-onecx.sh not found.${NC}\n"
   fi
 else
-  # Remove profile helper service, ignoring any error message
+  # Stop profile helper service, ignoring any error message
   docker compose down waiting-on-profile-$PROFILE > /dev/null 2>&1
 fi
 
